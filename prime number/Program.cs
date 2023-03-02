e98ddc0efd0ef53e99d12c13a939c6ee9e2ea394
unknown <amoljadhav>
2023-03-02 22:42:45 +0530

    [amol]add:prime number between 0 to 100

---
+++ prime number/Program.cs
@@ -0,0 +1,28 @@
+﻿// prime number between 0-100
+using System;
+
+{
+    static void Main(string[]) args)
+
+    {
+        Console.WriteLine("Prime numbers between 0 and 100:");
+
+        for (int i = 2; i <= 100; i++)
+
+        {
+            int factors = 0;
+
+            for (int j = 2; j <= i / 2; j++)
+            {
+                if (i % j == 0)
+
+                {
+                    factors++;
+                    break;
+
+                }
+            }
+            Console.Write(i + " ");
+        }
+    }
+}
